fc2de4751ff090e77a8448d04359da16bb904a64
Augusto Martinez <acm_asd@hotmail.com>
2020-01-06 16:20:56 -0600

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+//Show debug messages
+define ("DEBUG", true);
+
+//Database link
+define ("DBNAME", "webapp");
+define ("DBUSER", "root");
+define ("DBPASS", "");
+define ("DBHOST", "localhost");
+
+//Website URL and PATH
+define ("PATH", "//mvc-php/");
+define ("WEBSITE_TITLE", "Web app");
+
+//Default controller
+define ("DEFAULT_CONTROLLER", "home");
+?>

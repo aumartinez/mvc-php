3f7a706291df4bee8d0eb9e2f61a3e177564e8ed
Augusto Martinez <acm_asd@hotmail.com>
2020-01-07 14:19:27 -0600

    Update router.php

--- app/controllers/router.php
+++ app/controllers/router.php
@@ -1,9 +1,49 @@
 <?php
 
 class Router {
-  public static function route($url) {
-    //Code here..
-  }
+  protected $default_controller = "page";
+  protected $default_method = "index";
+  protected $params = [];
+
+  public function route($url) {
+    $url_array = array();
+    $url_array = explode("/", $url);
+    
+    if (in_array(PATH, $url_array)) {
+      while($url_array[0] != PATH) {
+        array_shift($url_array);
+      }
+    }
+    if ($url_array[0] == PATH) {
+      array_shift($url_array);
+    }
+    
+    $controller = isset($url_array[0]) ? array_shift($url_array) : "";    
+    $method = isset($url_array[0]) ? array_shift($url_array) : "";
+    $params = isset($url_array[0]) ? array_shift($url_array) : "";
+    
+    if (empty($controller)) {
+      $controller = $this->default_controller;
+    }
+    
+    if (empty($method)) {
+      $method = $this->default_method;
+    }
+    
+    $params = $url_array;
+    
+    $controller_name = $controller;
+    $controller = ucfirst($controller);
+    $dispatch = new $controller($controller_name, $method);
+    
+    if (method_exists($controller, $method)) {
+      call_user_func_array(array($dispatch, $method), $params);
+    }
+    else {
+      /* Error message */
+    }
+    
+  } 
 }
 
 ?>

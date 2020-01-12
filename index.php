bb358e7ef470f925049df1b849a3356e1541d382
Augusto Martinez <acm_asd@hotmail.com>
2020-01-12 15:03:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 define("DS", DIRECTORY_SEPARATOR);
 define("ROOT", dirname(__FILE__) . DS . "app");
 
-# Sanitize URL
+# Get URL from server and Sanitize URL
 $url = filter_var($_SERVER["REQUEST_URI"], FILTER_SANITIZE_URL);
 
 # Load core

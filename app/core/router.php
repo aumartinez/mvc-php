30f4d18c3bf8bca74402228314007180a1f26585
Augusto Martinez <acm_asd@hotmail.com>
2020-08-13 11:12:25 -0600

    Update router.php

--- app/core/router.php
+++ app/core/router.php
@@ -13,51 +13,77 @@ class Router {
     # Split url using "/" as separator
     $url_array = array();
     $url_array = explode("/", trim($url, "/"));
-    
+              
     # Remove app folder value
     if(in_array(PATH, $url_array)){
-      while ($url_array[0] != PATH) {
+      while ($url_array[0] != PATH) {        
         array_shift($url_array);
       }
       
       array_shift($url_array);
     }
-        
+            
     # If any, pass the corresponding controller, method and parameters
-    $controller = isset($url_array[0]) ? array_shift($url_array) : "";    
-    $method = isset($url_array[0]) ? array_shift($url_array) : "";
+    if (isset($url_array[0])){
+      # Extract parameters
+      $str = array_shift($url_array);      
+      $ind = strpos($str, "?");      
+      $str = $ind ? substr($str, 0, $ind): $str;
+      
+      $controller = $str;
+    }
+    else {
+      $constroller = "";
+    }
+    
+    if (isset($url_array[0])){
+      # Extract parameters
+      $str = array_shift($url_array);      
+      $ind = strpos($str, "?");
+      $str = $ind ? substr($str, 0, $ind): $str;
+      
+      $method = $str;
+    }
+    else {
+      $method = "";
+    }    
     
     # Pull URL query parameters if any
     $this->params = $url_array;
-    
+        
+                            
     # If controller is not found or not exists as a class handler
     # set default controller and not found method
     if (empty($controller)) {
       $controller = $this->default_controller;
     }
-    else if (!(class_exists($controller))) {
-      $controller = $this->default_controller;
-      $method = NOT_FOUND;      
-    }    
-    
+    else if (!is_controller($controller) || !class_exists($controller)) {
+      $controller = $this->default_controller;      
+      $method = NOT_FOUND;
+    }
+        
     if (empty($method)) {
       $method = $this->default_method;
     }
-    
+
+    # If hyphens are used in methods turn them into valid controller and method name
+    $controller = snake_case($controller);
+    $method = snake_case($method);    
+        
     # Instantiate controller class and call to appropriate method
     $controller_name = $controller;
     $controller = ucfirst($controller);
     $dispatch = new $controller($controller_name, $method);
-    
+        
     if (method_exists($controller, $method)) {
       call_user_func_array(array($dispatch, $method), $this->params);
     }
     else {
       # Error handler not found method
-      call_user_func_array(array($dispatch, NOT_FOUND), $this->params);      
+      call_user_func_array(array($dispatch, NOT_FOUND), $this->params);
     }
     
-  } 
+  }  
 }
 
 ?>

e30f52c7305683b6f7711842ffb594fdfac5a071
Augusto Martinez <acm_asd@hotmail.com>
2020-08-13 11:07:42 -0600

    Update functions.php

--- app/core/functions.php
+++ app/core/functions.php
@@ -2,4 +2,13 @@
 
 //Helper functions
 
+function is_controller($str) {
+  if (file_exists(ROOT . DS . "controllers" . DS . strtolower($str) . ".php")){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
 ?>

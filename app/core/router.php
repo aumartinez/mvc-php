213984365c9197298d94052512d657ab302f6d99
Augusto Martinez <acm_asd@hotmail.com>
2021-01-31 19:12:04 -0600

    Update router.php

--- app/core/router.php
+++ app/core/router.php
@@ -36,13 +36,22 @@ class Router {
       $constroller = "";
     }
     
+    $url_check = array();
+    $url_check = explode("/", trim($url, "/"));
+    $str = "app";
+              
+    if (in_array($str, $url_check)){
+      $controller = DEFAULT_CONTROLLER;
+      $method = NOT_FOUND;
+    }
+    
     if (isset($url_array[0])){
       # Extract parameters
       $str = array_shift($url_array);      
       $ind = strpos($str, "?");
       $str = $ind ? substr($str, 0, $ind): $str;
       
-      $method = $str;
+      $method = (!isset($method))?$str:$method;
     }
     else {
       $method = "";
@@ -50,8 +59,7 @@ class Router {
     
     # Pull URL query parameters if any
     $this->params = $url_array;
-        
-                            
+                                
     # If controller is not found or not exists as a class handler
     # set default controller and not found method
     if (empty($controller)) {
@@ -68,18 +76,18 @@ class Router {
 
     # If hyphens are used in methods turn them into valid controller and method name
     $controller = snake_case($controller);
-    $method = snake_case($method);    
+    $method = snake_case($method);      
         
     # Instantiate controller class and call to appropriate method
     $controller_name = $controller;
     $controller = ucfirst($controller);
     $dispatch = new $controller($controller_name, $method);
         
-    if (method_exists($controller, $method)) {
+    if (method_exists($controller, $method) && is_callable(array($controller, $method))) {
       call_user_func_array(array($dispatch, $method), $this->params);
     }
     else {
-      # Error handler not found method
+      # Error handler not found method, or method is private/protected
       call_user_func_array(array($dispatch, NOT_FOUND), $this->params);
     }
     
